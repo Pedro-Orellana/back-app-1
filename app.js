189d9cfb2e9e199c4695a97731eae123de873d72
Pedro Orellana <pedrorellana21@gmail.com>
2020-05-30 08:59:49 -0700

    cors support added

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ var mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
 var items = require('./routes/items')
 
+
+//CORS Support
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, POST, GET, PUT, DELETE, PATCH')
+    res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    res.setHeader('Access-Control-Allow-Credentials', true)
+        next();
+    })
+
+
 let mongoServer = "mongodb+srv://admin:adminPassword@cluster0-ratid.mongodb.net/test?retryWrites=true&w=majority"
 
 mongoose.connect(mongoServer, {useNewUrlParser: true, useUnifiedTopology: true}).then(
@@ -24,5 +35,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/items', items);
+
+
+
+
 
 module.exports = app;
